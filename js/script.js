2a8da6957c5b78bcac51d5e41b1b34d2bfe0a15e
KauaRodrigues-Dev <kauarodrigues041@gmail.com>
2026-03-12 15:20:04 -0300

    feat: add animations and JavaScript functionality

---
+++ js/script.js
@@ -0,0 +1,83 @@
+//todo Método para deixar o link do menu ativo
+
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+//? ////////////////////////////////////////////////////////////////////////////////////////////
+
+//todo ativar itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(parametro);
+}
+
+parametros.forEach(ativarProduto);
+
+//? ////////////////////////////////////////////////////////////////////////////////////////////
+
+//todo Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarpergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarpergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//? ////////////////////////////////////////////////////////////////////////////////////////////
+
+//todo Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//? ////////////////////////////////////////////////////////////////////////////////////////////
+
+//todo Animção Simples
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
+
+//? ////////////////////////////////////////////////////////////////////////////////////////////
